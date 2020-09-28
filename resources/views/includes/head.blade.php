34d090c1b1c730510000c44ab0c0dca7a55672d3
rpgwaiter <rpgwaiter@based.zone>
2020-09-28 18:29:57 -0500

    Added short name (env) to title

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex">
-    <title>BFH
+    <title>{{env('SHORT_NAME')}}
         @yield('req', '')
     </title>
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" type="text/css">
