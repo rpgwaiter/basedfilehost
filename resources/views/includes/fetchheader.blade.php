f26a74d541822c48b72bf048e436c56a65d2b6fe
rpgwaiter <rpgwaiter@based.zone>
2020-09-29 14:13:15 -0500

    Cleaned up styling and dependencies

--- resources/views/includes/fetchheader.blade.php
+++ resources/views/includes/fetchheader.blade.php
@@ -1,4 +1,4 @@
-<div class="alert" role="alert">
-    <h4 class="alert-heading">Showing {{count(get_object_vars($dirs))}} folders, {{$controller::human_filesize($totalsize)}}
-        in {{count(get_object_vars($files))}} files @ {{$filereq}}</h4>
+<div class="page-header py-3">
+    <h3>Showing {{count(get_object_vars($dirs))}} folders, {{$controller::human_filesize($totalsize)}}
+        in {{count(get_object_vars($files))}} files @ {{$filereq}}</h3>
 </div>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -4,6 +4,7 @@
     <title>{{env('SHORT_NAME')}}
         @yield('req', '')
     </title>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" type="text/css">
     <script src="{{ URL::asset('js/app.js') }}"></script>
     <script src="{{ URL::asset('js/filehost.js') }}"></script>
